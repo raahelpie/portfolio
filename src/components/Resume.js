7608f11d3d152a5d0dc8f523ce94f3fd147f4609
raahelpie <17131a05h5@gvpce.ac.in>
2020-05-13 00:32:17 +0530

    resume link updated

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ const Resume = () => {
   return (
     <div>
       <a
-        href='https://drive.google.com/file/d/1DIjYAbuAkwfG4toLqOwTUZ1n0U3BoYiw/view'
+        href='https://drive.google.com/file/d/1DIjYAbuAkwfG4toLqOwTUZ1n0U3BoYiw/view?usp=sharing'
         target='_blank'
       >
         <p className='sectionTitle' style={{ color: 'rgb(0, 0, 0)' }}>
