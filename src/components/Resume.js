bc1ff213e4fc755f84ea1e9fddf81472f4759614
Raahel Baig <59283826+raahelpie@users.noreply.github.com>
2023-01-05 13:56:07 +0530

    updated resume link

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ const Resume = () => {
   return (
     <div>
       <a
-        href='https://drive.google.com/file/d/1HonfN3imrcMe5Qn_-jJs00JFpKgkpDCm/view?usp=sharing'
+        href='https://drive.google.com/file/d/1o74e0yXmRzxgcIypxY1NZPDZZTF_wQMs/view?usp=sharing'
         target='_blank'
       >
         <p className='sectionTitle' style={{ color: 'rgb(0, 0, 0)' }}>
